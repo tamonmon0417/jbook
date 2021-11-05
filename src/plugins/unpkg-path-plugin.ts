2b367b8a02e7f9f9ddcdaa4348d8345b98a4ff14
tamonmon0417 <tamonmon0417@gmail.com>
2021-11-05 22:09:22 +0900

    url constructor

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,5 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios'
+import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
   return {
@@ -9,36 +9,46 @@ export const unpkgPathPlugin = () => {
         console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
-        } 
+        }
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, args.importer + '/').href,
+          };
+        }
 
         return {
           namespace: 'a',
-          path: `https://unpkg.com/${args.path}`
-        }
-        
+          path: `https://unpkg.com/${args.path}`,
+        };
+
         // else if (args.path === 'tiny-test-pkg') {
-        //   return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a',}
+        //   return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //     namespace: 'a',
+        //   };
         // }
       });
- 
+
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
- 
+
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'tiny-test-pkg';
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };
         }
-        
-        const {data} = await axios.get(args.path)
+
+        const { data } = await axios.get(args.path);
         return {
           loader: 'jsx',
-          contents: data
-        }
+          contents: data,
+        };
       });
     },
   };
