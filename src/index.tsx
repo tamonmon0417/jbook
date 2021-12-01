ede823bed703d404b73c087f7cd8593a5a056b6f
tamonmon0417 <tamonmon0417@gmail.com>
2021-12-01 21:35:30 +0900

    add iframe

--- src/index.tsx
+++ src/index.tsx
@@ -39,6 +39,12 @@ const App = () => {
     console.log(result)
 
     setCode(result.outputFiles[0].text)
+
+    try {
+      eval(result.outputFiles[0].text)
+    } catch (err) {
+      alert(err)
+    }
   }
 
   return (
@@ -51,6 +57,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe sandbox="allow-same-origin" src="/test.html"></iframe>
     </div>
   )
 }
